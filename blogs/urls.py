01f0859bcfc4de9a6b0fe7434a7739262f708e39
siumhossain <sium.hossain@yahoo.com>
2021-05-21 00:44:02 +0600

    before auth

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('blogs/', views.snippet_list),
+    path('blogs/', views.blog_list),
+    path('blogs/<int:pk>/', views.blog_detail),
     
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,21 +1,46 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from .models import Blog
 from .serializers import BlogSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-@csrf_exempt
-def snippet_list(request):
+
+
+
+@api_view(['GET', 'POST'])
+def blog_list(request):
     
     if request.method == 'GET':
         Blogs = Blog.objects.all()
         serializer = BlogSerializer(Blogs, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
         serializer = BlogSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data,status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def blog_detail(request,pk):
+    try:
+        blog = Blog.objects.get(pk=pk)
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        #print(data)
+        serializer = BlogSerializer(blog,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
